df6ad80535f8a8e523248f4820c9602093fa0587
p.corbel <p.corbel@e-deal.com>
2017-04-11 19:04:10 +0200

    Create not working test

--- src/app/component/common/form/ed-form-text-input-component/ed-form-text-input-component.component.spec.ts
+++ src/app/component/common/form/ed-form-text-input-component/ed-form-text-input-component.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { EdFormTextInputComponent } from './ed-form-text-input-component.component';
 import {FormsModule} from "@angular/forms";
 import {EdRessourceFactory, EdUnknownObjectResource} from "../../../../services/dao/ressource/ressource.impl";
+import {DataDictionnary} from "../../../../services/dao/ressource/datadictionary.impl";
 
 describe('EdFormTextInputComponent', () => {
   let component: EdFormTextInputComponent;
@@ -29,5 +30,33 @@ describe('EdFormTextInputComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display property value', () => {
+    fixture = TestBed.createComponent(EdFormTextInputComponent);
+    component = fixture.componentInstance;
+    debugger;
+    const resource = new EdUnknownObjectResource(null, null, {
+        type: null,
+        propertyName: null,
+        ownerObjectDef: null,
+        objectDef: DataDictionnary.getInstance().getObjectDefinition("Person"),
+        isMultival: false
+    });
+    resource.setProperty("PerName", "toto");
+
+    component.resource = resource;
+    component.attributeName = "PerName";
+
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector("input").value).toEqual("toto");
+
+    /*
+     const fixture = TestBed.createComponent(AppComponent);
+     fixture.detectChanges();
+     const compiled = fixture.debugElement.nativeElement;
+     expect(compiled.querySelector('h1').textContent).toContain('app works!');
+     */
   });
 });
