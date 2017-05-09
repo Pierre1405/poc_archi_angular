241bf249aad3e3da2e68d5b9879cb8323f71e15d
p.corbel <p.corbel@e-deal.com>
2017-05-09 11:44:20 +0200

    Start using generic for collection + First busines bean implement test

--- src/app/services/dao/store/store.interface.ts
+++ src/app/services/dao/store/store.interface.ts
@@ -1,15 +1,14 @@
 import {EdDaoICollectionRessource, EdDaoIObjectResource} from "../ressource/resource.interface";
 import {Observable} from "rxjs/Observable";
 
-export interface EdDaoIStore {
+export interface EdDaoIStore<C extends EdDaoICollectionRessource<O>, O extends EdDaoIObjectResource> {
   readResource(resource: EdDaoIObjectResource): Observable<EdDaoIObjectResource>;
-  readCollection(resource: EdDaoICollectionRessource, filter: any, order: any, pagination: any): Observable<EdDaoICollectionRessource>;
-  saveResources(resources: (EdDaoIObjectResource|EdDaoICollectionRessource)[]):
-                              Observable<(EdDaoIObjectResource|EdDaoICollectionRessource)[]>;
+  readCollection(resource: C, filter: any, order: any, pagination: any): Observable<C>;
+  saveResources(resources: (O|C)[]):Observable<(O|C)[]>;
 }
 
 
-export interface FieldAdapter  {
+export interface ResourceAdapter<T extends EdDaoIObjectResource> {
   persistanceToApplication (rawData: any): any;
   applicationToPersistance (rawData: any): any;
 }
